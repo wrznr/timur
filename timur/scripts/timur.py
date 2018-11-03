2f2819075e3d9ae197fe6a4d0c2b2d14f09afe6a
Kay-Michael Würzner <wuerzner@2txt.de>
2018-11-03 21:13:22 +0100

    Add symbol tables to unions and concatenations
    @kylebgorman suggested a simple recipe for adding symbol tables
    to union and concatenation operations which has been applied
    here.
    
    Cf. https://github.com/kylebgorman/Pynini/issues/8

--- timur/scripts/timur.py
+++ timur/scripts/timur.py
@@ -1,6 +1,23 @@
 # -*- coding: utf-8 -*- 
 import pynini
 
+def union(*args, token_type="utf8"):
+    args_mod = []
+    for arg in args:
+        if type(args) == "str":
+            args_mod.append(pynini.acceptor(arg, token_type=token_type))
+        else:
+            args_mod.append(arg)
+    return pynini.union(*(args_mod))
+
+def concat(*args, token_type="utf8"):
+    args_mod = []
+    conc = pynini.Fst()
+    for arg in args:
+        if type(args) == "str":
+            arg = pynini.acceptor(arg, token_type=token_type)
+        conc = pynini.concat(conc, arg)
+    return conc
 
 def load_alphabet(source, auto_singletons=True):
     '''
@@ -32,6 +49,15 @@ def construct_any(symbol_table):
         sym_it.next()
     return ANY
 
+def phon_fst(symbol_table):
+    '''
+    Orthographic and phonological surface realizations rules
+    '''
+    cons_lower = pynini.string_map(["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z", "ß"], input_token_type=symbol_table, output_token_type=symbol_table)
+    cons_upper = pynini.string_map(["B", "C", "D", "F", "G", "H", "J", "K", "L", "M", "N", "P", "Q", "R", "S", "T", "V", "W", "X", "Y", "Z"], input_token_type=symbol_table, output_token_type=symbol_table)
+    return cons.optimize()
+
+
 def num_fst(symbol_table):
     '''
     Definition of cardinal and ordinal number stems
@@ -40,118 +66,115 @@ def num_fst(symbol_table):
     #
     # cardinal number stems
     #
-    card_stem_3_9 = pynini.union("drei", "vier", "fünf", "sechs", "sieben", "acht", "neun")
-    card_stem_2_9 = pynini.union("zwei", "zwo", card_stem_3_9)
-    card_stem_1_9 = pynini.union("eins", card_stem_2_9)
-    num_stem_10_19 = pynini.union("zehn", "elf", "zwölf", pynini.concat(card_stem_3_9, "zehn"))
-    num_stem_20_90 = pynini.union("dreißig", pynini.concat(pynini.union("zwan", "vier", "fünf", "sech", "sieb", "acht", "neun"), "zig"))
+    und = pynini.acceptor("u n d", token_type=symbol_table)
+    card_stem_3_9 = union("drei", "vier", "fünf", "sechs", "sieben", "acht", "neun", token_type=symbol_table)
+    card_stem_2_9 = union("zwei", "zwo", card_stem_3_9, token_type=symbol_table)
+    card_stem_1_9 = union("eins", card_stem_2_9, token_type=symbol_table)
+    num_stem_10_19 = union("zehn", "elf", "zwölf", concat(card_stem_3_9, "zehn", token_type=symbol_table), token_type=symbol_table)
+    num_stem_20_90 = union("dreißig", concat(union("zwan", "vier", "fünf", "sech", "sieb", "acht", "neun", token_type=symbol_table), "zig", token_type=symbol_table), token_type=symbol_table)
 
-    card_2_99 = pynini.union(
+    card_2_99 = union(
             card_stem_2_9,
             num_stem_10_19,
-            pynini.concat(
-                pynini.concat(
-                    pynini.union("ein", card_stem_2_9), "und"
-                    ).closure(0,1),
+            concat(
+                concat(union("ein", card_stem_2_9, token_type=symbol_table), "und", token_type=symbol_table).closure(0,1),
                 num_stem_20_90)
             )
-    card_1_99 = pynini.union("eins", card_2_99)
+    card_1_99 = union("eins", card_2_99, token_type=symbol_table)
 
-    card_2_999 = pynini.union(
+    card_2_999 = union(
             card_2_99,
-            pynini.concat(
-                pynini.union("ein", card_stem_2_9).closure(0,1),
-                pynini.concat(
+            concat(
+                union("ein", card_stem_2_9, token_type=symbol_table).closure(0,1),
+                concat(
                     "hundert",
-                    pynini.concat(
-                        pynini.acceptor("und").closure(0,1),
-                        card_1_99
-                        ).closure(0,1)
+                    concat(
+                        und.closure(0,1),
+                        card_1_99, token_type=symbol_table
+                        ).closure(0,1),
+                    token_type=symbol_table
                     )
                 )
             )
-    card_1_999 = pynini.union("eins", card_2_999)
+    card_1_999 = union("eins", card_2_999, token_type=symbol_table)
 
-    card_2_999999 = pynini.union(
+    card_2_999999 = union(
             card_2_999,
-            pynini.concat(
-                pynini.union("ein", card_2_999).closure(0,1),
-                pynini.concat(
+            concat(
+                union("ein", card_2_999, token_type=symbol_table).closure(0,1),
+                concat(
                     "tausend",
-                    pynini.concat(
-                        pynini.acceptor("und").closure(0,1),
-                        card_1_999
-                        ).closure(0,1)
+                    concat(
+                        und.closure(0,1),
+                        card_1_999, token_type=symbol_table
+                        ).closure(0,1),
+                    token_type=symbol_table
                     )
                 )
             )
-    card_base = pynini.union("null", "eins", card_2_999999)
-    card_deriv = pynini.union("null", "ein", card_2_999999)
+    card_base = union("null", "eins", card_2_999999, token_type=symbol_table)
+    card_deriv = union("null", "ein", card_2_999999, token_type=symbol_table)
 
     #
     # ordinal number stems
     #
-    ord_stem_3_9 = pynini.union("dritt", "viert", "fünft", "sechst", "siebt", "acht", "neunt")
-    ord_stem_1_9 = pynini.union("erst", "zweit", ord_stem_3_9)
+    ord_stem_3_9 = union("dritt", "viert", "fünft", "sechst", "siebt", "acht", "neunt", token_type=symbol_table)
+    ord_stem_1_9 = union("erst", "zweit", ord_stem_3_9, token_type=symbol_table)
 
-    ord_3_99 = pynini.union(
+    ord_3_99 = union(
             ord_stem_3_9,
-            pynini.concat(num_stem_10_19, "t"),
-            pynini.concat(num_stem_20_90, "st"),
-            pynini.concat(
-                pynini.union("ein", card_stem_2_9),
-                pynini.concat(
-                    "und",
-                    pynini.concat(num_stem_20_90, "st")
-                    )
+            concat(num_stem_10_19, "t", token_type=symbol_table),
+            concat(num_stem_20_90, "st", token_type=symbol_table),
+            concat(
+                union("ein", card_stem_2_9, token_type=symbol_table),
+                concat("und", num_stem_20_90, "st", token_type=symbol_table)
                 )
             )
-    ord_1_99 = pynini.union("erst", "zweit", ord_3_99)
+    ord_1_99 = union("erst", "zweit", ord_3_99, token_type=symbol_table)
 
-    ord_3_999 = pynini.union(
+    ord_3_999 = union(
             ord_3_99,
-            pynini.concat(
-                pynini.union("ein", card_stem_2_9).closure(0, 1),
-                "hundertst"
+            concat(
+                union("ein", card_stem_2_9, token_type=symbol_table).closure(0, 1),
+                "hundertst",
+                token_type=symbol_table
                 ),
-            pynini.concat(
-                pynini.union("ein", card_stem_2_9).closure(0, 1),
-                pynini.concat(
-                    pynini.concat("hundert", pynini.acceptor("und").closure(0, 1)),
-                    ord_1_99)
+            concat(
+                union("ein", card_stem_2_9, token_type=symbol_table).closure(0, 1),
+                concat(
+                    "hundert",
+                    und.closure(0, 1),
+                    ord_1_99,
+                    token_type=symbol_table)
                 )
             )
-    ord_1_999 = pynini.union("erst", "zweit", ord_3_999)
+    ord_1_999 = union("erst", "zweit", ord_3_999, token_type=symbol_table)
 
-    ord_3_999999 = pynini.union(
+    ord_3_999999 = union(
             ord_3_999,
-            pynini.concat(
-                pynini.union("ein", card_2_999).closure(0, 1),
-                pynini.concat(
-                    pynini.concat("tausend", pynini.acceptor("und").closure(0, 1)),
-                    ord_1_999)
+            concat(
+                union("ein", card_2_999, token_type=symbol_table).closure(0, 1),
+                concat("tausend", und.closure(0, 1), ord_1_999, token_type=symbol_table)
                 )
             )
-    ord_base = pynini.union("nullt", "erst", "zweit", ord_3_999999)
+    ord_base = union("nullt", "erst", "zweit", ord_3_999999, token_type=symbol_table)
 
     #
     # numbers expressed with digits
     #
-    dig_card = pynini.string_map([str(x) for x in range(0, 10)]).closure(1)
-    dig_ord = pynini.concat(dig_card, ".")
+    dig_card = pynini.string_map([str(x) for x in range(0, 10)], input_token_type=symbol_table, output_token_type=symbol_table).closure(1)
+    dig_ord = concat(dig_card, ".", token_type=symbol_table)
 
     #
     # adding morphosyntactic information
     #
     quant_helper = pynini.transducer("<QUANT>", "", input_token_type=symbol_table)
-    quant = pynini.union(
-            pynini.concat(card_deriv, quant_helper),
-            pynini.concat(ord_base, quant_helper),
-            pynini.concat(
-                pynini.concat(dig_card, pynini.acceptor("-").closure(0,1)),
-                quant_helper),
-            pynini.concat(
-                pynini.union("beid", "mehr", "viel", "dies", "doppel", "ganz", "gegen"),
+    quant = union(
+            concat(card_deriv, quant_helper),
+            concat(ord_base, quant_helper),
+            concat(dig_card, pynini.acceptor("-").closure(0,1), quant_helper),
+            concat(
+                union("beid", "mehr", "viel", "dies", "doppel", "ganz", "gegen", token_type=symbol_table),
                 quant_helper)
             )
 
@@ -159,49 +182,45 @@ def num_fst(symbol_table):
     ord_features = pynini.transducer("", "<ORD> <base> <nativ> <Ord>",  output_token_type=symbol_table)
     #
     # resulting base stems
-    num_base = pynini.concat(
+    num_base = concat(
             pynini.transducer("", "<Initial> <Base_Stems>", output_token_type=symbol_table),
-            pynini.union(
-                pynini.concat(card_base, card_features),
-                pynini.concat(dig_card, card_features),
-                pynini.concat(ord_base, ord_features),
-                pynini.concat(dig_ord, ord_features)
+            union(
+                concat(card_base, card_features),
+                concat(dig_card, card_features),
+                concat(ord_base, ord_features),
+                concat(dig_ord, ord_features)
                 )
             )
     
     #
     # resulting deriv stems
-    num_deriv = pynini.concat(
+    num_deriv = concat(
             pynini.transducer("", "<Deriv_Stems>", output_token_type=symbol_table),
-            pynini.concat(
-                pynini.union(
-                    pynini.concat(card_deriv, pynini.acceptor("<CARD>", token_type=symbol_table)),
-                    pynini.concat(dig_card, pynini.acceptor("<DIGCARD>", token_type=symbol_table)),
-                    pynini.concat(ord_base, pynini.acceptor("<ORD>", token_type=symbol_table))
-                    ),
-                pynini.transducer("", "<deriv> <nativ>", output_token_type=symbol_table)
-                )
+            union(
+                concat(card_deriv, "<CARD>", token_type=symbol_table),
+                concat(dig_card, "<DIGCARD>", token_type=symbol_table),
+                concat(ord_base, "<ORD>", token_type=symbol_table)
+                ),
+            pynini.transducer("", "<deriv> <nativ>", output_token_type=symbol_table)
             )
     
     #
     # resulting kompos stems
-    num_kompos = pynini.concat(
+    num_kompos = concat(
             pynini.transducer("", "<Kompos_Stems>", output_token_type=symbol_table),
-            pynini.concat(
-                ord_base,
-                pynini.concat(
-                    pynini.acceptor("<ORD>", token_type=symbol_table),
-                    pynini.transducer("", "<deriv> <nativ>", output_token_type=symbol_table)
-                    )
-                )
+            ord_base,
+            "<ORD>",
+            pynini.transducer("", "<deriv> <nativ>", output_token_type=symbol_table),
+            token_type=symbol_table
             )
     
     return pynini.union(num_base, num_deriv, num_kompos).optimize()
 
 syms = load_alphabet(open("syms.txt"))
 
+phon = phon_fst(syms)
+phon.draw("test.dot")
 num_stems = num_fst(syms)
-num_stems.draw("test.dot")
 
 ANY = construct_any(syms)
 

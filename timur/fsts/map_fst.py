19991b03c9cd6fcc1163fd366ca091a71f1df012
Kay-Michael Würzner <wuerzner@2txt.de>
2018-11-07 21:43:06 +0100

    Add inflection classes and complex lexicon entry features

--- timur/fsts/map_fst.py
+++ timur/fsts/map_fst.py
@@ -86,6 +86,71 @@ def map_fst_map1(symbol_table):
 
     # complexity agreement features
     complexity_agreement_feats = pynini.string_map(["<simplex>", "<komposit>", "<suffderiv>", "<prefderiv>"], input_token_type=symbol_table, output_token_type=symbol_table)
+
+    # complex lexicon entries
+    complex_lex_entries = pynini.string_map(["<Simplex>", "<Komplex>", "<Komplex_abstrakt>", "<Komplex_semi>", "<Nominalisierung>", "<Kurzwort>"], input_token_type=symbol_table, output_token_type=symbol_table)
+
+    # inflection classes
+    infl_classes = pynini.string_map([
+      "<Abk_ADJ>", "<Abk_ADV>", "<Abk_ART>", "<Abk_DPRO>",
+      "<Abk_KONJ>", "<Abk_NE-Low>", "<Abk_NE>", "<Abk_NN-Low>",
+      "<Abk_NN>", "<Abk_PREP>", "<Abk_VPPAST>", "<Abk_VPPRES>",
+      "<Adj$>", "<Adj$e>", "<Adj+(e)>", "<Adj+>", "<Adj&>",
+      "<Adj+Lang>", "<Adj+e>", "<Adj-el/er>", "<Adj0>",
+      "<Adj0-Up>", "<AdjComp>", "<AdjSup>", "<AdjNN>",
+      "<AdjNNSuff>", "<AdjPos>", "<AdjPosAttr>", "<AdjPosPred>",
+      "<AdjPosSup>", "<AdjSup>", "<Adj~+e>",
+      "<Adv>",
+      "<Circp>",
+      "<FamName_0>", "<FamName_s>", "<Name-Pl_0>", "<Name-Pl_x>", 
+      "<Intj>", "<IntjUp>",
+      "<Konj-Inf>", "<Konj-Kon>", "<Konj-Sub>", "<Konj-Vgl>",
+      "<N?/Pl_0>", "<N?/Pl_x>", 
+      "<NFem-Deriv>", "<NFem-a/en>", "<NFem-in>", "<NFem-is/en>",
+      "<NFem-is/iden>", "<NFem-s/$sse>", "<NFem-s/sse>", "<NFem-s/ssen>",
+      "<NFem/Pl>", "<NFem/Sg>", "<NFem_0_$>", "<NFem_0_$e>", "<NFem_0_e>", 
+      "<NFem_0_en>", "<NFem_0_n>", "<NFem_0_s>", "<NFem_0_x>",
+      "<NGeo+er/in>", "<NGeo-Fem_0>", "<NGeo-Invar>", "<NGeo-Masc_0>",
+      "<NGeo-Masc_s>", "<NGeo-Neut+Loc>", "<NGeo-Neut_0>", 
+      "<NGeo-Neut_s>", "<NGeo-Pl_0>",
+      "<NMasc-Adj>", "<NMasc-ns>", "<NMasc-s/$sse>", "<NMasc-s/Sg>",
+      "<NMasc-s/sse>", "<NMasc-s0/sse>", "<NMasc-us/en>", "<NMasc-us/i>",
+      "<NMasc/Pl>", "<NMasc/Sg_0>", "<NMasc/Sg_es>", "<NMasc/Sg_s>",
+      "<NMasc_0_x>", "<NMasc_en_en=in>", "<NMasc_en_en>", "<NMasc_es_$e>", 
+      "<NMasc_es_$er>", "<NMasc_es_e>", "<NMasc_es_en>", "<NMasc_n_n=$in>",
+      "<NMasc_n_n=in>", "<NMasc_n_n>", "<NMasc_s_$>", "<NMasc_s_$x>",
+      "<NMasc_s_0=in>", "<NMasc_s_0>", "<NMasc_s_e=in>", "<NMasc_s_e>",
+      "<NMasc_s_en=in>", "<NMasc_s_en>", "<NMasc_s_n>", "<NMasc_s_s>",
+      "<NMasc_s_x>", 
+      "<NNeut-0/ien>", "<NNeut-Dimin>", "<NNeut-Herz>", "<NNeut-a/ata>",
+      "<NNeut-a/en>", "<NNeut-on/a>", "<NNeut-s/$sser>", "<NNeut-s/sse>",
+      "<NNeut-um/a>", "<NNeut-um/en>", "<NNeut/Pl>", "<NNeut/Sg_0>", 
+      "<NNeut/Sg_en>", "<NNeut/Sg_es>", "<NNeut/Sg_s>", "<NNeut_0_x>",
+      "<NNeut_es_$e>", "<NNeut_es_$er>", "<NNeut_es_e>", "<NNeut_es_en>",
+      "<NNeut_es_er>", "<NNeut_s_$>", "<NNeut_s_0>", "<NNeut_s_e>",
+      "<NNeut_s_en>", "<NNeut_s_n>", "<NNeut_s_s>", "<NNeut_s_x>",
+      "<Name-Fem_0>", "<Name-Fem_s>", "<Name-Masc_0>", "<Name-Masc_s>",
+      "<Name-Neut_s>", "<Name-Neut_0>", "<Name-Neut+Loc>", "<Name-Invar>", 
+      "<Postp-Akk>", "<Postp-Dat>", "<Postp-Gen>",
+      "<Pref/Adj>", "<Pref/Adv>", "<Pref/N>", "<Pref/ProAdv>", "<Pref/Sep>",
+      "<Pref/V>", "<Prep-Akk>", "<Prep-Dat>", "<Prep-Gen>", "<Prep/Art-m>",
+      "<Prep/Art-n>", "<Prep/Art-r>", "<Prep/Art-s>",
+      "<ProAdv>",
+      "<PInd-Invar>",
+      "<Ptkl-Adj>", "<Ptkl-Ant>", "<Ptkl-Neg>", "<Ptkl-Zu>",
+      "<VAImpPl>", "<VAImpSg>", "<VAPastKonj2>", "<VAPres1/3PlInd>", 
+      "<VAPres1SgInd>", "<VAPres2PlInd>", "<VAPres2SgInd>", "<VAPres3SgInd>",
+      "<VAPresKonjPl>", "<VAPresKonjSg>",
+      "<VInf+PPres>", "<VInf>",
+      "<VMPast>", "<VMPastKonj>", "<VMPresPl>", "<VMPresSg>", 
+      "<VPPast>", "<VPPres>", "<VPastIndReg>", "<VPastIndStr>",
+      "<VPastKonjStr>", "<VPresKonj>", "<VPresPlInd>",
+      "<VVPP-en>", "<VVPP-t>", "<VVPastIndReg>", "<VVPastIndStr>",
+      "<VVPastKonjReg>", "<VVPastKonjStr>", "<VVPastStr>", "<VVPres1+Imp>",
+      "<VVPres1>", "<VVPres2+Imp0>", "<VVPres2+Imp>", "<VVPres2>",
+      "<VVPres2t>", "<VVPres>", "<VVPresPl>", "<VVPresSg>",
+      "<VVReg-el/er>", "<VVReg>", "<WAdv>"], input_token_type=symbol_table, output_token_type=symbol_table)
+
     return pynini.Fst()
 
 def map_fst_map2(symbol_table):

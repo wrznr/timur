d44cfb563e2f4bf7b24d495edbf9ebb012b2266a
Kay-Michael Würzner <wuerzner@2txt.de>
2019-01-20 17:09:59 +0100

    Add general infrastructure

--- timur/fsts/__init__.py
+++ timur/fsts/__init__.py
@@ -3,5 +3,6 @@ from .phon_fst import phon_fst
 from .inflection_fst import InflectionFst
 from .map_fst import MapFst
 from .deko_fst import DekoFst
+from .defaults_fst import DefaultsFst
 from .timur_fst import TimurFst
 from .sublexica import Sublexica
